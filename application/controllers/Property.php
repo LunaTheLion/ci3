35c1de7923ec74d3de77024433136ffb497239a6
Elona Mae <elonasquared@gmail.com>
2019-08-22 19:05:52 +0800

    upload single file
    complete

--- application/controllers/Property.php
+++ application/controllers/Property.php
@@ -11,48 +11,100 @@ class Property extends CI_CONTROLLER
 	}
 	public function sample_upload()
 	{
-		$target_dir = "uploads/";
+		$data = array(
+			'property_code' => $this->session->userdata('property_code'),
+			'property_type' => $this->session->userdata('property_type'),
+			'property_address' => $this->session->userdata('property_address'),
+			'property_building' => $this->session->userdata('property_building'),
+			
+			'property_bath' => $this->session->userdata('property_bath'),
+			'property_parking' => $this->session->userdata('property_parking'),
+			'property_floor_area' => $this->session->userdata('property_floor_area'),
+			'property_lot_area' => $this->session->userdata('property_lot_area'),
+			'property_pet' => $this->session->userdata('property_pet'),
+			'property_garden' => $this->session->userdata('property_garden'),
+
+			'property_amenities' => $this->session->userdata('property_amenities'),
+			'property_date_posted' =>date('F j, Y  g:i'),
+
+			'property_additional_details'=> $this->input->post('property_additional_details'),
+			'property_price' => $this->input->post('property_price'),
+			'property_status' => $this->input->post('property_status'),
+			'property_title' => $this->input->post('propertyTitle'),
+			'property_title_slug' => urlencode($this->input->post('propertyTitle')),
+		);
+
+		$title = urlencode($this->input->post('propertyTitle'));
+		$target_dir = "uploads/".$title."/facade/";
+
 		$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-		$uploadOk = 1;
 		$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-		// Check if image file is a actual image or fake image
-		if(isset($_POST["submit"])) {
-		    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-		    if($check !== false) {
-		        echo "File is an image - " . $check["mime"] . ".";
-		        $uploadOk = 1;
-		    } else {
-		        echo "File is not an image.";
-		        $uploadOk = 0;
-		    }
-		}
-		// Check if file already exists
-		if (file_exists($target_file)) {
-		    echo "Sorry, file already exists.";
-		    $uploadOk = 0;
-		}
-		// Check file size
-		if ($_FILES["fileToUpload"]["size"] > 500000) {
-		    echo "Sorry, your file is too large.";
-		    $uploadOk = 0;
-		}
-		// Allow certain file formats
-		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-		&& $imageFileType != "gif" ) {
-		    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-		    $uploadOk = 0;
-		}
-		// Check if $uploadOk is set to 0 by an error
-		if ($uploadOk == 0) {
-		    echo "Sorry, your file was not uploaded.";
-		// if everything is ok, try to upload file
-		} else {
-		    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-		        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-		    } else {
-		        echo "Sorry, there was an error uploading your file.";
-		    }
-		}
+		$file = $_FILES["fileToUpload"]["name"];
+		//check if there is an existing folder
+		$dir = "uploads/".$title."/facade/";
+		
+					if(!is_dir($dir))
+					{
+						mkdir($dir,0755,true);
+						if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) 
+						{
+						   // echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+						    
+						}
+						
+					}
+					elseif(is_dir($dir))
+					{
+						if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) 
+						{
+						    //echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+						    
+						}
+						
+						
+					}	
+
+
+
+
+		
+		//  else {
+		//     echo "Sorry, there was an error uploading your file.";
+		// }
+		// // Check if image file is a actual image or fake image
+		// if(isset($_POST["submit"])) {
+		//     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+		//     if($check !== false) {
+		//         echo "File is an image - " . $check["mime"] . ".";
+		//         $uploadOk = 1;
+		//     } else {
+		//         echo "File is not an image.";
+		//         $uploadOk = 0;
+		//     }
+		// }
+		// // Check if file already exists
+		// if (file_exists($target_file)) {
+		//     echo "Sorry, file already exists.";
+		//     $uploadOk = 0;
+		// }
+		// // Check file size
+		// if ($_FILES["fileToUpload"]["size"] > 500000) {
+		//     echo "Sorry, your file is too large.";
+		//     $uploadOk = 0;
+		// }
+		// // Allow certain file formats
+		// if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+		// && $imageFileType != "gif" ) {
+		//     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+		//     $uploadOk = 0;
+		// }
+		// // Check if $uploadOk is set to 0 by an error
+		// if ($uploadOk == 0) {
+		//     echo "Sorry, your file was not uploaded.";
+		// // if everything is ok, try to upload file
+		// } else {
+		   
+		// }
 		
 	}
 	public function read_all()
